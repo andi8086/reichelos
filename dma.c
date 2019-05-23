85dabc050616d9b5941e8f77b3ea224481b1c187
Andreas Reichel <andreas.reichel.ext@siemens.com>
2019-05-23 18:02:13 +0200

    Improve dma.c

--- dma.c
+++ dma.c
@@ -3,9 +3,28 @@
 
 // intel 8237A dma controller
 
+void DMA_mask_channel(uint8_t channel)
+{
+	if (channel < 4) {
+		outb(DMA_SCM_0_3, (1 << 2) | channel);
+	} else {
+		// FIXME
+	}
+}
+
+void DMA_unmask_channel(uint8_t channel)
+{
+	if (channel < 4) {
+		outb(DMA_SCM_0_3, (0 << 2) | channel);
+	} else {
+		// FIXME
+	}
+}
+
+
 void DMA_floppy_init_mem(uint32_t phys_mem_addr, uint16_t count)
 {
-	outb(DMA_SCM_0_3, 0x06); // mask DMA channel 2 and 0 (assuming 0 is already masked)
+	DMA_mask_channel(2);
 	outb(DMA_FLIPFLOP_0_3, 0xFF); // reset flipflop
 	outb(DMA_START_ADDR_2, phys_mem_addr & 0xFF);
 	outb(DMA_START_ADDR_2, (phys_mem_addr >> 8) & 0xFF);
@@ -15,23 +34,23 @@ void DMA_floppy_init_mem(uint32_t phys_mem_addr, uint16_t count)
 	outb(DMA_COUNT_2, count >> 8);
 
 	outb(DMA_PAGE_2, (phys_mem_addr >> 16) & 0xFF);
-	outb(DMA_SCM_0_3, 0x02); // unmask DMA channel 2
+	DMA_unmask_channel(2);
 }
 
 void DMA_floppy_write()
 {
-	outb(DMA_SCM_0_3, 0x06);
+	DMA_mask_channel(2);
 	outb(DMA_MODE_0_3, 0x5A); //DMAM_SINGLE_TRANS | DMAM_ADDR_INC | DMAM_AUTOINIT | DMAM_WRITE | 2);
-	outb(DMA_SCM_0_3, 0x02);
+	DMA_unmask_channel(2);
 }
 
 // FIXME: autoinit??
 
 void DMA_floppy_read()
 {
-	outb(DMA_SCM_0_3, 0x06);
+	DMA_mask_channel(2);
 	outb(DMA_MODE_0_3, 0x56);
-	outb(DMA_SCM_0_3, 0x02);
+	DMA_unmask_channel(2);
 }
 
 
